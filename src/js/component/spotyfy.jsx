509dd80fa1d8c0abc54ddf2a21ad3fb1ebddd192
danseks <danseksfzz@gmail.com>
2021-01-30 17:13:20 +0000

    Add style, bottons and more...

--- src/js/component/spotyfy.jsx
+++ src/js/component/spotyfy.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
-import { propTypes } from "react-bootstrap/esm/Image";
-import PropTypes from "prop-types";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
 
-export function Songlist(props) {
+export function Songlist() {
 	const [songs, setSongs] = useState([]);
 	const [urlSong, setUrlSong] = useState("");
-	const [nextSong, setNextSong] = useState("");
-	const getAudioID = document.querySelector("#audioSong");
+	const [actualSong, setActualSong] = useState("");
 	const [pause, setPause] = useState(false);
 	const songURL = "https://assets.breatheco.de/apis/sound/";
+	const audioRef = useRef();
 
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/sound/all")
@@ -17,56 +16,109 @@ export function Songlist(props) {
 			})
 			.then(jsonSong => {
 				let listOfSongs = Object.values(jsonSong)[1];
-				//console.log(listOfSongs);
 				setSongs(listOfSongs);
 			});
 	}, []);
 
 	const trackListOfSong = () => {
 		return songs.map((item, index) => (
-			<button
+			<li
 				id={index}
 				key={index.toString()}
 				onClick={() => {
 					setPause(false);
 					setUrlSong(item.url);
-					setNextSong(index);
+					setActualSong(index);
 				}}
 				name={item.name}>
 				{item.name}
-			</button>
+			</li>
 		));
 	};
 
 	const updateSong = () => {
 		if (pause == false) {
-			getAudioID.pause();
+			audioRef.current.pause();
 			setPause(true);
 		} else {
-			getAudioID.play();
+			audioRef.current.play();
+			setPause(false);
+		}
+	};
+
+	const nextSong = actual => {
+		let initialSong = 0;
+		if (actual == 21) {
+			setUrlSong(songs[0].url);
+			setActualSong(initialSong);
+			setPause(false);
+		} else {
+			setUrlSong(songs[actual + 1].url);
+			setActualSong(actual + 1);
+			setPause(false);
+		}
+	};
+
+	const previusSong = actual => {
+		let finalSong = 21;
+		if (actual == 0) {
+			setUrlSong(songs[21].url);
+			setActualSong(finalSong);
+			setPause(false);
+		} else {
+			setUrlSong(songs[actual - 1].url);
+			setActualSong(actual - 1);
 			setPause(false);
 		}
 	};
 
 	return (
 		<>
-			<div>{trackListOfSong()}</div>
-			<div>
-				{" "}
-				<audio
-					controls
-					autoPlay
-					key={nextSong}
-					src={songURL + urlSong}
-					id="audioSong"
-				/>{" "}
-			</div>
-			<button onClick={() => updateSong()}>pausa</button>
+			<audio
+				controls
+				autoPlay
+				key={actualSong}
+				src={songURL + urlSong}
+				ref={audioRef}
+				className="d-none"
+			/>
+			<Container>
+				<Card className="mx-auto mt-5">
+					<Card.Body>
+						<Card.Title>
+							<Row className="row-player">
+								<Col>
+									<Button
+										onClick={() => previusSong(actualSong)}
+										variant="default"
+										className="uvs-left">
+										<i className="fa fa-backward ml-4" />
+									</Button>
+								</Col>
+								<Col>
+									{" "}
+									<Button
+										onClick={() => updateSong()}
+										variant="default"
+										className="uvs-left">
+										<i className="fa fa-play ml-3" />
+									</Button>
+								</Col>
+								<Col>
+									<Button
+										onClick={() => nextSong(actualSong)}
+										variant="default"
+										className="uvs-left">
+										<i className="fa fa-forward ml-3" />
+									</Button>
+								</Col>
+							</Row>
+						</Card.Title>
+					</Card.Body>
+					<progress max="100" value="100" />
+					<ol className="list-group">{trackListOfSong()}</ol>
+				</Card>
+			</Container>{" "}
 		</>
 	);
 }
-
-Songlist.propTypes = {
-	name: PropTypes.string,
-	url: PropTypes.string
-};
